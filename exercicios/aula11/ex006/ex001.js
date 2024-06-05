7a0c3ee3e393e2063376426debcab937f02b253b
Goulart <143452337+egoulart1423@users.noreply.github.com>
2024-06-04 23:21:30 -0300

    novos exercícios e comentários

---
+++ exercicios/aula11/ex006/ex001.js
@@ -0,0 +1,6 @@
+var vel = 99.5
+console.log(`À velocidade do seu aotomóvel é de ${vel}!`)
+if (vel > 90){
+  console.log(`Você foi multado por está acima do limite de velocidade!`)
+}
+console.log(`Dirija sempre com a mão no volante e a outra no cachimbo!`)
---
+++ exercicios/aula11/ex006/ex002.js
@@ -0,0 +1,7 @@
+var pais = "brasil"
+
+if (pais != "brasil"){
+  console.log(`Sai daqui alienígena!!`)
+} else {
+  console.log(`Tu és brazuca!!`)
+}
