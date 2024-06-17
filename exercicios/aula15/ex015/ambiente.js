6a76eceaefcfd5badc3dcc3dbcb410ec3e9afe77
Goulart <143452337+egoulart1423@users.noreply.github.com>
2024-06-17 00:25:29 -0300

    while e do

--- exercicios/aula15/ex015/ambiente.js
+++ exercicios/aula15/ex015/ambiente.js
@@ -1,12 +1,17 @@
+
+//teste de repetição por ultimo
 var c = 1
-while (c <= 6) {
+do {
   console.log('boa noite!')
+  c++
+} while (c <= 6) 
+
+//teste de repetição primeiro
+var c = 1
+while (c <= 3) {
+  console.log('boa noite!')
+  c++
 }
-
-
-
-
-
 
 /* console.log('boa noite!')
 console.log('boa noite!')
