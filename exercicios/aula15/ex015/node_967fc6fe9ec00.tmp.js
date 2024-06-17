b22474ea787b4aaf697256d6bafaefd6d8d3fbc5
Goulart <143452337+egoulart1423@users.noreply.github.com>
2024-06-17 00:14:32 -0300

    afsawdsa

---
+++ exercicios/aula15/ex015/node_967fc6fe9ec00.tmp.js
@@ -0,0 +1,16 @@
+var c = 1
+while (c <= 6) {
+  console.log('boa noite!')
+}
+
+
+
+
+
+
+/* console.log('boa noite!')
+console.log('boa noite!')
+console.log('boa noite!')
+console.log('boa noite!')
+console.log('boa noite!')
+console.log('boa noite!') */
