ad4df2c686f71944cc19aaf58e90cf993bb39798
Goulart <143452337+egoulart1423@users.noreply.github.com>
2024-07-12 03:34:19 -0300

    for com in

---
+++ exercicios/aula18/ex20/ambiente.js
@@ -0,0 +1,13 @@
+function parimp(n) {
+  if (n % 2 == 0) {
+    return 'O número é par'
+  } else {
+    return 'O número é impar'
+  }
+}
+/*
+let res = parimp(11)
+console.log(res)
+*/
+
+console.log(parimp(232))
