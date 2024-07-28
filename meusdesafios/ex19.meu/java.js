0dfa8897e236cdc2b37bfa65a4b3940eb24a3605
Goulart <143452337+egoulart1423@users.noreply.github.com>
2024-07-28 02:04:29 -0300

    colocando a função finalizar
    primeiro num, maior num, menor num e a quantidade de num add

--- meusdesafios/ex19.meu/java.js
+++ meusdesafios/ex19.meu/java.js
@@ -1,21 +1,41 @@
+let res = document.getElementById("res")
+let dados = []
+
 function enviar() {
-  let res = document.getElementById("res")
   let num = document.getElementById("txtn")
   let n = Number(num.value)
-
-  if (num.value.length == 0) {
+  
+  if (num.value == "") {
     window.alert('Bota o número parça')
   } else if (n < 1 || n > 100) {
     window.alert('De 1 a 100 parça não intendeu?')
   } else {
-    res.innerHTML += `Valor ${n} adicionado <br>`
+    dados.push(n)
+    res.innerHTML = `${dados}`
   }
+
+  num.value = ''
 }
 
-function finalizar() {
-  let res = document.getElementById("res")
-  let f = document.getElementById("final")
-  
-  let numeros = res.innerText.length
-  f.innerHTML = `você adicionou ${numeros}`
+function finalizar() {  
+  let menornum = dados[0]
+  let maiornum = dados[0]
+
+  for(let i = 1; i < dados.length; i++) {
+    if (dados[i] > maiornum) {
+      maiornum = dados[i]
+    }
+  }
+
+  for(let e = 1; e < dados.length; e++) {
+    if (dados[e] < menornum) {
+      menornum = dados[e]
+    }
+  }
+
+  let quantidade = dados.length
+  document.getElementById('final').innerText = `Você adicionou ${quantidade} números!`
+  document.getElementById('final').innerHTML += `<br>O maior número adicionado é ${maiornum}` 
+  document.getElementById('final').innerHTML += `<br>O menor número adicionado é ${menornum}`
+  document.getElementById('final').innerHTML += `<br>O primeiro valor adicionado é ${dados[0]}`
 }
